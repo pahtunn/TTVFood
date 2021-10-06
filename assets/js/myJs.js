06f4474a0fea4e5f2d4edf74e0809d50cccfe62e
pahtunn <tuancode2402200@gmail.com>
2021-10-06 17:33:16 +0700

    Add Sign in, Sign up

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,3 +1,5 @@
+
+// Food categories
 let foodMenuList = document.querySelector('.food-item-wrap');
 let foodCategory = document.querySelector('.food-category');
 let categories = foodCategory.querySelectorAll('button');
@@ -10,3 +12,38 @@ Array.from(categories).forEach((item) => {
         foodMenuList.classList = 'food-item-wrap ' + e.target.getAttribute('data-food-type')
     }
 })
+
+// Login modal
+// Sign im
+var modal = document.getElementById("signInModal");
+var btnSignIn = document.getElementById("btnSignIn");
+var btnClose = document.getElementsByClassName("close")[0];
+
+btnSignIn.onclick = function () {
+    modal.style.display = "block";
+}
+
+btnClose.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Sign up
+var modal2 = document.getElementById("signUpModal");
+var btnSignUp = document.getElementById("btnSignUp");
+var btnClose2 = document.getElementsByClassName("close2")[0];
+
+btnSignUp.onclick = function () {
+    modal2.style.display = "block";
+}
+
+btnClose2.onclick = function () {
+    modal2.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
